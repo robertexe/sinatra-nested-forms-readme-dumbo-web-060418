8735483d20ff527b5e5172a21fcde9ba81730868
robertexe <robtabares92@gmail.com>
2018-06-28 08:44:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,25 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+
+	get '/' do
+	  erb :new
+	end
+
+	post '/student' do
+		puts params
+
+		## create an instance of Student taking in params[:student] which is the form
+  @student = Student.new(params[:student])
+
+  params[:student][:courses].each do |details|
+    Course.new(details)
+		redirect to "/"
+  end
+
+  @courses = Course.all
+
+  erb :student
+	end
+
 end
